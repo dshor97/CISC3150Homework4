9a4f29a38054101419284a243d3205f3f0fbc2dd
Dayton Shor <dayton.shor@gmain.com>
2017-09-19 18:38:32 -0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,25 @@ public class Main {
     public static void main(String[] args){
         Scanner number = new Scanner(System.in);
 
+        System.out.println("Pick an n for the n-Queen problem: ");
         int num = number.nextInt();
 
-        nQueen(num, new int [num][num]);
+        nQueen(num);
     }
-    public static void nQueen(int num, int[][] a){
-        
+    public static void nQueen(int num){
+        int[][] board = new int[num][num];
+        printBoard(board,num);
+    }
+    public static void printBoard(int[][] board, int num){
+        for(int i = 0; i<num; i++){
+            for(int j = 0;j<num;j++){
+                if(board[i][j] <= 0) {
+                    System.out.print(" * ");
+                }else {
+                    System.out.print(" Q ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
