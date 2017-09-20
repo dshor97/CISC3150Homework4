babbb4fa67592c9fbbab1480696332f7ee15591e
Dayton Shor <dayton.shor@gmain.com>
2017-09-20 13:39:57 -0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -16,13 +16,30 @@ public class Main {
     public static void printBoard(int[][] board, int num){
         for(int i = 0; i<num; i++){
             for(int j = 0;j<num;j++){
-                if(board[i][j] <= 0) {
-                    System.out.print(" * ");
-                }else {
+                if(board[i][j] == 0) {
+                    System.out.print(" O ");
+                }else if(board[i][j] < 0) {
+                    System.out.print(" X ");
+                }else{
                     System.out.print(" Q ");
                 }
             }
             System.out.println();
         }
+    }
+    public static int[][] addQueen(int x, int y,int size){
+        int[][] temp = new int[size][size];
+        temp[x][y] += 1;
+        for(int i = 0;i < size;i++){
+            if(i!= y){
+                temp[x][i] -= 1;
+            }
+        }
+        for(int i = 0;i < size;i++){
+            if(i!= x){
+                temp[i][y] -= 1;
+            }
+        }
+        return temp;
     }
 }
