c8bd4f24d2c15c9c9b7a5179482d7183ea8c4cd4
Dayton Shor <dayton.shor@gmain.com>
2017-09-20 14:25:35 -0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,10 @@ public class Main {
     public static void nQueen(int num){
         int[][] board = new int[num][num];
         printBoard(board,num);
+
+        board = addQueen(1,1,num,board);
+
+        printBoard(board,num);
     }
     public static void printBoard(int[][] board, int num){
         for(int i = 0; i<num; i++){
@@ -26,18 +30,31 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
     }
-    public static int[][] addQueen(int x, int y,int size){
+    public static int[][] addQueen(int x, int y,int size, int a [][]){
         int[][] temp = new int[size][size];
-        temp[x][y] += 1;
+        a[x][y] = 1;
         for(int i = 0;i < size;i++){
             if(i!= y){
-                temp[x][i] -= 1;
+                a[x][i] -= 1;
             }
         }
         for(int i = 0;i < size;i++){
             if(i!= x){
-                temp[i][y] -= 1;
+                a[i][y] -= 1;
+            }
+        }
+        int row = x, col = y;
+        while(row !=0 || col!=0){
+            row--;
+            col--;
+        }
+
+        //copy a into temp
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                temp[i][j] = a[i][j];
             }
         }
         return temp;
