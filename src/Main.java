6c468bb8ee3eac5f515a7cc03fe0424a1acc0869
Dayton Shor <dayton.shor@gmain.com>
2017-09-23 16:09:09 -0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -10,45 +10,22 @@ public class Main {
         nQueen(num);
     }
     public static void nQueen(int num) {
-        Queue<Board> que = new LinkedList<Board>();
-        ArrayList<Board> solutions = new ArrayList<Board>();
-        que.add(new Board(num));
-        int i = 0;
-        while(i<num){
-            while(!que.isEmpty()) {
-                Board b = que.remove();
-                for (int j = 0; j < num; j++) {
-                    if (b.board[i][j] == 0) {
-                        solutions.add(b.addQueen(i, j, num, b.board));
-                    }
-                }
-            }
-            int j = 0;
-            while (!solutions.isEmpty()) {
-                que.add(solutions.remove(j));
-                j++;
-            }
-            i++;
+        int size = num;
+        int n = 1;
+        ArrayList<Board> b = new ArrayList<Board>();
+        for(int i = 0; i<b.size(); i++){
+            b.add(new Board(num));
         }
+        while(n < num){
+            Board[] board = new Board[num];
+            int sizeb = 0;
+
+        }
+
+
     }
 
-}
-class Board{
-    int[][] board;
-
-    Board(int num){
-        board = new int[num][num];
-    }
-    Board(int[][] board){
-        int temp[][] = new int[board.length][board.length];
-        for(int i =0; i<board.length; i++){
-            for(int j = 0; j<board.length; j++){
-                temp[i][j] = board[i][j];
-            }
-        }
-        this.board = temp;
-    }
-    public void printBoard(int[][] board){
+    public static void printBoard(int[][] board){
         for(int i = 0; i<board.length; i++){
             for(int j = 0;j<board.length;j++){
                 if(board[i][j] == 0) {
@@ -63,18 +40,20 @@ class Board{
         }
         System.out.println();
     }
-
-    public Board addQueen(int x, int y,int size, int a [][]){
+    public static void printBoard(Board b){
+        printBoard(b.board);
+    }
+    public static Board addQueen(int x, int y,int size, Board board){
         int[][] temp = new int[size][size];
-        a[x][y] = 1;
+        board.board[x][y] = 1;
         for(int i = 0;i < size;i++){
             if(i!= y){
-                a[x][i] -= 1;
+                board.board[x][i] -= 1;
             }
         }
         for(int i = 0;i < size;i++){
             if(i!= x){
-                a[i][y] -= 1;
+                board.board[i][y] -= 1;
             }
         }
         int row1 = x, col1 = y;
@@ -85,7 +64,7 @@ class Board{
 
         while(row1 < size && col1 < size){
             if(row1 != x && col1 != y) {
-                a[row1++][col1++] -= 1;
+                board.board[row1++][col1++] -= 1;
             }else{
                 row1++;
                 col1++;
@@ -100,7 +79,7 @@ class Board{
 
         while(row2 < size && col2 >= 0){
             if(row2 != x && col2 != y) {
-                a[row2++][col2--] -= 1;
+                board.board[row2++][col2--] -= 1;
             }else{
                 row2++;
                 col2--;
@@ -109,10 +88,42 @@ class Board{
 
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j++){
-                temp[i][j] = a[i][j];
+                temp[i][j] = board.board[i][j];
             }
         }
         return new Board(temp);
     }
 }
+
+class Board {
+    int[][] board;
+
+    Board(int num) {
+        board = new int[num][num];
+    }
+
+    Board(int[][] board) {
+        int temp[][] = new int[board.length][board.length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                temp[i][j] = board[i][j];
+            }
+        }
+        this.board = temp;
+    }
+
+    public String toString() {
+        String temp = new String();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == 1) {
+                    temp += "( " + i + ',' + j + " ) ,";
+                }
+            }
+        }
+        return temp;
+    }
+}
+
+
 
